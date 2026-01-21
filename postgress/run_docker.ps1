fe6d5ae58bcfe3b250b126ca2a414e0c386a6677
MRJacek <mateuszjacek93@gmail.com>
2026-01-21 21:10:31 +0100

    fix volume patch, in postregess 18 version there is ennother patch level, withous /data

--- postgress/run_docker.ps1
+++ postgress/run_docker.ps1
@@ -1,10 +1,9 @@
-#!/bin/bash
 # Budowanie obrazu
 
 docker run -it --rm `
   -e POSTGRES_USER="root" `
   -e POSTGRES_PASSWORD="root" `
   -e POSTGRES_DB="ny_taxi" `
-  -v "ny_taxi_postgres_data:/var/lib/postgresql/data" `
+  -v "ny_taxi_postgres_data:/var/lib/postgresql" `
   -p 5432:5432 `
   postgres:18
