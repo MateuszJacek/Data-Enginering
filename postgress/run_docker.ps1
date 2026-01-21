79f9869dc234d39ce3492f54c01a53d186ac391a
MRJacek <mateuszjacek93@gmail.com>
2026-01-21 21:00:30 +0100

    fix script by removing unnecesaary comment

--- postgress/run_docker.ps1
+++ postgress/run_docker.ps1
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Budowanie obrazu
 
-docker run -it --rm ` # run docker container
-  -e POSTGRES_USER="root" ` # set postgres user
-  -e POSTGRES_PASSWORD="root" ` # set postgres password
-  -e POSTGRES_DB="ny_taxi" ` # set default database
-  -v "ny_taxi_postgres_data:/var/lib/postgresql/data" ` # persist data
-  -p 5432:5432 ` # map port
-  postgres:18 # specify image
+docker run -it --rm `
+  -e POSTGRES_USER="root" `
+  -e POSTGRES_PASSWORD="root" `
+  -e POSTGRES_DB="ny_taxi" `
+  -v "ny_taxi_postgres_data:/var/lib/postgresql/data" `
+  -p 5432:5432 `
+  postgres:18
