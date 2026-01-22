831e3a2549bfc9094d435097aebbd48f3150d56c
MRJacek <mateuszjacek93@gmail.com>
2026-01-22 11:31:33 +0100

    add parameters to ingestion script

--- docker/postgres_new_york_taxi/ingest_data.py
+++ docker/postgres_new_york_taxi/ingest_data.py
@@ -1,6 +1,7 @@
 # converted from notebook.ipynb by script:uv run jupyter nbconvert --to=script notebook.ipynb
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -28,21 +29,18 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of data to ingest')
+@click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--chunksize', default=100000, type=int, help='Number of rows per chunk to process')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-def run():
-
-    year = 2021
-    month = 1
-
-    pg_user = 'root'
-    pg_pass ='root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize=100000
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz'
