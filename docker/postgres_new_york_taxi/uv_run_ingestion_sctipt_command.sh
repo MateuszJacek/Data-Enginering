591752cb2995784b981c27542fc2d816602b1adb
MRJacek <mateuszjacek93@gmail.com>
2026-01-22 11:34:36 +0100

    add uv_run_ingestion_script_dommand

---
+++ docker/postgres_new_york_taxi/uv_run_ingestion_sctipt_command.sh
@@ -0,0 +1,10 @@
+uv run python ingest_data.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000 \
+  --target-table=yellow_taxi_trips
